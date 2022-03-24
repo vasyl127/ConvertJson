473d7ef6a31bf94b4e931a48a00508f9bc03b801
vasyl.marmash <vasyl.marmash@teamvoy.com>
2022-03-24 12:28:00 +0200

    edit app.rb

--- app.rb
+++ app.rb
@@ -1,18 +1,6 @@
 require_relative 'convertor'
 include Convertor
 
-
 json = JSON.parse(File.open('q.json').read)
 
-header_keys = head_colums array_with_rows(json)
-out_array = array_with_rows json
-
-CSV.open('test_csv.csv', 'w') do |csv|
-  csv << header_keys.keys
-  out_array.each do |row|
-    csv << header_keys.merge(row).values
-  end
-end
-
-
-# puts convert_json_to_csv(json)
+puts Convertor::convert_json_to_csv(json)
